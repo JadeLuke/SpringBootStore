95e69169f334a5c0c3ffd9f196dd8a85dbc9b6b8
Nellark <nelakulati@gmail.com>
2025-06-13 09:35:30 +0200

    added entity class

--- src/main/java/com/mvp/store/storeApp/entities/Supplier.java
+++ src/main/java/com/mvp/store/storeApp/entities/Supplier.java
@@ -25,9 +25,13 @@ public class Supplier {
 
     @Column
     String contact;
-
+    // This defines a one-to-many relationship: one Supplier can supply many items.
+    // - 'mappedBy = "supplier"' means the 'supplier' field in the Store class owns the relationship.
+    // - 'cascade = CascadeType.ALL' means any changes to Supplier (like save/delete) will apply to its Stores.
+    // - 'orphanRemoval = true' means if a Store is removed from the list, it will be deleted
     @OneToMany(mappedBy = "supplier", cascade = CascadeType.ALL, orphanRemoval = true)
-    List<Store> items;
+    List<Store> items; // A list that holds all the items (stores) supplied by the supplier
 }
 
 
+// "Owns the relationship" means the 'supplier' field in the Store class controls the database link
